77b49a4907e777bd54597529a2774f24ad07cceb
ykangxie <xyk0755@gmail.edu>
2013-03-09 12:59:45 -0800

    all.year (wrong SMF)

--- hwk4.R
+++ hwk4.R
@@ -16,110 +16,162 @@
 #sapply(cmd,system)
 
 ##############################################
-# (I) Counts
+# PART I. Counts
 ##############################################
 files = gsub(".*([0-9]{4}.csv).*", "\\1", list.files()[grep(".*([0-9]{4}.csv).*", list.files())])
 year = gsub(".*([0-9]{4}).csv.*", "\\1", files[grep(".*([0-9]{4}.csv).*", files)])
-year = as.character(1987:2008)
 
-#############################
-# TIME: counts for 2008.csv
-#############################
+################################################
+# Exploration- system.time(counts) for 2008.csv
+################################################
 setwd("./Desktop/airport")
 
-#(1) Shell Scrpiting
+dept=c("LAX","OAK","SFO","SMF")
 year="2008"
-shcount<-function(year){
-  cmd<-sprintf("for airport in LAX OAK SFO SMF; do cut -f 17 -d , %s.csv | grep $airport | wc -l; done",year)
-  temp<-lapply(cmd,function(command) system(command,intern=TRUE))
-  SHcounts<-lapply(temp,function(x)structure(as.numeric(x),names=c("LAX","OAK","SFO","SMF")));
-  names(SHcounts)<-year
-  SHcounts
-}
-system.time(SHcounts2008<-shcount("2008"))
-Time.SHcounts<-.Last.value
 
-#notes:for each year.csv, we count the number of flights leaving each of the airports
+#(I) Shell Scripting
+#1.1 /grep & wc -l/
+system.time(SHcounts.wc<-system("for airport in LAX OAK SFO SMF; do cut -f 17 -d , 2008.csv | grep $airport | wc -l; done",intern=TRUE))
+system.time(SHcounts.wc<-structure(as.numeric(SHcounts.wc),names=c("LAX","OAK","SFO","SMF")))
 
-#(2) R Streaming
-rcount<-function(B,csvfile){
+#user  system elapsed 
+#67.317   0.867  55.346 
+
+#1.2 /egrep & (sort + uniq -c)/
+system.time(SHcounts.uniq<-system("egrep '([0-9]|NA),(LAX|OAK|SFO|SMF),[A-Z]' 2008.csv | cut -f 17 -d , | sort | uniq -c",intern=TRUE))
+
+#user  system elapsed 
+#61.342   0.185  60.458 
+
+#(II) R Streaming 
+rstreaming<-function(B,csvfile="2008.csv"){
   B=as.integer(B)
   con=file(csvfile,"r")
-  LAX=0;OAK=0;SFO=0;SMF=0;
+  Rcounts<-structure(c(data=rep(0,4)),names=c("LAX","OAK","SFO","SMF"))
   while(TRUE){
     txt=readLines(con,n=B)
     if (length(txt)==0)
       break
-    temp=sapply(strsplit(txt,","),"[[",17) 
-    LAX = LAX + length(temp[(temp=="LAX")])
-    OAK = OAK + length(temp[(temp=="OAK")])
-    SFO = SFO + length(temp[(temp=="SFO")])
-    SMF = SMF + length(temp[(temp=="SMF")])
+    temp=sapply(strsplit(txt,","),"[[",17)
+    update<-as.numeric(table(temp)[names(Rcounts)])
+    update[is.na(update)]<-0
+    Rcounts<-Rcounts+update
   }
-  Rcounts<-cbind(LAX,OAK,SFO,SMF)
-  row.names(Rcounts)<-year
   Rcounts
 }
 
-# find global optimal B
-checkTime<-sapply(c(1000,5000,10000,50000,100000),
-                  function(B){
-                    csvfile="2008.csv"
-                    system.time(rcount(B,csvfile))
-                    }
-                  )
-options(scipen=999)
-plot(c(1000,5000,10000,50000,100000),checkTime[3,],)
+system.time(rstreaming(400L,"2008.csv")) #261.543   1.146 262.663
+system.time(rstreaming(12000L,"2008.csv")) #257.718   0.922 258.620 
 
-# find local optimal B around 100000
-checkTime.local<-sapply(c(8000,12000,14000,16000,18000),
-                  function(B){
-                    csvfile="2008.csv"
-                    system.time(rcount(B,csvfile))
-                  }
-)
-plot(c(5000,8000,12000,14000,16000,18000,10000),c(268.263,checkTime.local[,3],258.497))
 
-#8000
-system.time(rcount(8000,"2008.csv"))
+#(III) R Streaming from Unix
+con<-pipe("egrep '([0-9]|NA),(LAX|OAK|SFO|SMF),[A-Z]' 2008.csv","r")
+rcount<-function(B,con,year){
+  B=as.integer(B)
+  Rcounts<-structure(c(data=rep(0,4)),names=c("LAX","OAK","SFO","SMF"))
+  while(TRUE){
+    txt=readLines(con,n=B)
+    if (length(txt)==0)
+      break
+    temp=sapply(strsplit(txt,","),"[[",17)
+    update<-as.numeric(table(temp)[names(Rcounts)])
+    update[is.na(update)]<-0
+    Rcounts<-Rcounts+update
+  }
+  Rcounts
+}
+system.time(RSHcount<-rcount(400L,con,"2008"))
 #user  system elapsed 
-#261.613   1.010 262.640
-
-#12000
-system.time(rcount(12000,"2008.csv"))
-#user  system elapsed 
-#255.807   0.799 256.567 
-
-#20000
-system.time(rcount(20000,"2008.csv"))
-#user  system elapsed 
-#264.036   0.712 264.747 
-
-plot(c(5000,seq(8000,20000,by=2000),c(22000,24000,26000),c(28000,30000,32000)),c(268.263,262.640,258.497,256.567,checkTime.local[3,],264.747,checkTime.local2[3,],checkTime.local3[3,]),xlab="B lines",ylab=("elapsed.time (secs)"))
-
-#ii. Rprof the best "B"
-Sys.setlocale(locale="C")
-Rprof("Rstream.out")
-rcount(5000,2008.csv)
-Rprof(NULL)
-summaryRprof("Rstream.out")
-
-#(3) Rprof Compare
-summaryRprof("Shell.out")
-summaryRprof("Rstream.out")
+#19.476   0.061  57.478 
 
 ##############################################
-# (II) Mean and Std.Dev
+# (II) Mean and Std.Dev for 2008.csv
 ##############################################
-#system("egrep '(LAX|SFO|OAK|SMF)' [12]*.csv > CalFlights")
-#system("tail -f Counts")
-year = as.character(1987:2008)
+con<-pipe("egrep '([0-9]|NA),(LAX|OAK|SFO|SMF),[A-Z]' 2008.csv","r")
+rstats<-function(B,con,year){
+  B=as.integer(B)
+  #set up counters
+  Rcounts<-structure(c(data=rep(0,4)),names=c("LAX","OAK","SFO","SMF"))
+  Rsum<-structure(c(data=rep(0,4)),names=c("LAX","OAK","SFO","SMF"))
+  Rsumvar<-structure(c(data=rep(0,4)),names=c("LAX","OAK","SFO","SMF"))
+  #reading blocks
+  while(TRUE){
+    txt=readLines(con,n=B)
+    if (length(txt)==0)
+      break
+    #ArrDelay & Airport
+    ArrDelay=as.numeric(sapply(strsplit(txt,","),"[[",15))
+    Origin=as.factor(sapply(strsplit(txt,","),"[[",17))
+    #dropNA
+    dropNA<-!is.na(ArrDelay)
+    ArrDelay<-ArrDelay[dropNA]
+    Origin<-Origin[dropNA]
+    #1.1 Rcounts
+    update<-as.numeric(table(Origin)[names(Rcounts)])
+    update[is.na(update)]<-0
+    Rcounts<-Rcounts+update
+    #1.2 Rsum
+    update<-tapply(ArrDelay,Origin,sum)[names(Rsum)]
+    update[is.na(update)]<-0
+    Rsum<-Rsum+update
+    #1.3 Rvar
+    update<-tapply(ArrDelay,Origin,function(x){var(x)*length(x)})[names(Rsumvar)]
+    update[is.na(update)]<-0
+    Rsumvar<-Rsumvar+update   
+  }
+  cbind(Rcounts,Rsum,Rsumvar)
+}
 
-#Shell
-cat 2008.csv | cut -f 17 -d , | egrep '(LAX|OAK|SFO|SMF)' | sort | uniq | wc -l
-cmean2<-system("egrep [0-9],LAX 2008.csv | cut -f 15 -d , | awk 'BEGIN {s=0; c=0}; {s=s+$1;c=c+1}; END {print c,s/c}'",intern=TRUE)
-#> cmean
+system.time(RS.ALL<-rstats(400L,con))
+#user  system elapsed 
+#34.124   0.099  62.476 
+RS[,2]/RS[,1]
+sqrt(RS[,3]/RS[,1])
+
+# awk validation
+system.time(cmean<-system("egrep '([0-9]|NA),LAX,[A-Z]' 2008.csv | cut -f 15 -d , | awk 'BEGIN {s=0; c=0}; {s=s+$1;c=c+1}; END {print c,s/c}'",intern=TRUE))
+#> counts mean (LAX 2008.csv)
 #[1] "181706 5.28919"
+#[1] "52886 2.05015"
+#[1] "116306 10.5643"
+#[1] "44990 3.32407"
+#user  system elapsed for each airport
+#41.994   0.185  41.596 
+#43.088   0.216  43.191 
+system.time(cmean2<-system("egrep '([0-9]|NA),LAX,[A-Z]' [12]*.csv | cut -f 15 -d , | awk 'BEGIN {s=0; c=0}; {s=s+$1;c=c+1}; END {print c,s/c}'",intern=TRUE))
+# counts mean (LAX [12]*.csv)
+#[1] "3947365 5.90915"
 
-#R
-con=pipe(cut -f 17 d , | grep LAX|SFO|SMF|OAK| )
+
+##############################################
+# (II) Mean and Std.Dev for All Years
+##############################################
+Sys.setlocale(locale="C")
+#ALL YEAR
+year = gsub(".*([0-9]{4}).csv.*", "\\1", files[grep(".*([0-9]{4}.csv).*", files)])
+cmds<-sprintf("egrep '([0-9]|NA),(LAX|OAK|SFO|SMF),[A-Z]' %s.csv",year)
+#temp<-lapply(cmds,system,intern=TRUE)
+
+RS.ALL.ARRAY<-lapply(cmds,function(cmd){con<-pipe(cmd,"r")
+                                        tmp<-rstats(400L,con)
+                                        close(con)
+                                        tmp},
+                     simplify=True)
+names(RS.ALL.ARRAY)<-year
+
+con<-pipe("egrep '([0-9]|NA),(LAX|OAK|SFO|SMF),[A-Z]' [12]*.csv",open="r")
+system.time(RS.ALL<-rstats(400L,con))
+#user   system  elapsed 
+#763.809    2.189 1332.127 
+
+#1987
+con<-pipe("egrep '([0-9]|NA),(LAX|OAK|SFO|SMF),[A-Z]' 1987.csv",open="r")
+system.time(RS.1987<-rstats(400L,con))
+#table
+#user  system elapsed 
+#10.749   0.025  19.469 
+#tapply
+#user  system elapsed 
+#8.725   0.040  15.688 
+RS[,2]/RS[,1]
+sqrt(RS[,3]/RS[,1])
